d2d75d7eccc38becad332b021fec7f21e03e8838
OmarEDC <omarulises.escueladecodigo@gmail.com>
2021-04-27 15:30:58 -0500

    Se añade JavaScript en plantilla, falta optimizar el algoritmo

---
+++ js/functions.js
@@ -0,0 +1,49 @@
+function niñoArc(){
+    var foto=document.getElementById("fotoArc");
+    foto.src="./img/ArcturusBB.jpeg";    
+}
+
+function actualArc(){
+    var foto=document.getElementById("fotoArc");
+    foto.src="./img/Arcturus.jpeg";
+}
+
+function niñaMarlene(){
+    var foto=document.getElementById("fotoMarlene");
+    foto.src="./img/MarleneBB.jpeg";    
+}
+
+function actualMarlene(){
+    var foto=document.getElementById("fotoMarlene");
+    foto.src="./img/Marlene.jpeg";
+}
+
+function niñaMangie(){
+    var foto=document.getElementById("fotoMangie");
+    foto.src="./img/MangieBB.jpeg";    
+}
+
+function actualMangie(){
+    var foto=document.getElementById("fotoMangie");
+    foto.src="./img/Mangie.jpeg";
+}
+
+function niñoOmar(){
+    var foto=document.getElementById("fotoOmar");
+    foto.src="./img/OmarBB.jpg";    
+}
+
+function actualOmar(){
+    var foto=document.getElementById("fotoOmar");
+    foto.src="./img/Omar.jpeg";
+}
+
+function niñaItzel(){
+    var foto=document.getElementById("fotoItzel");
+    foto.src="./img/ItzelBB.jpeg";    
+}
+
+function actualItzel(){
+    var foto=document.getElementById("fotoItzel");
+    foto.src="./img/Itzel.jpeg";
+}
